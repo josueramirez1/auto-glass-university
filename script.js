3614cff6e30c74725ffa9021b752dc5cab58a134
josueramirez1 <josue67ramirez@gmail.com>
2026-07-10 16:17:27 -0700

    implemented mobile navigation menu

--- script.js
+++ script.js
@@ -14,14 +14,30 @@ tl.to(".hero__title", { opacity: 1, y: 0 })
   );
 
 // --- 2. SCROLL ANIMATION (Triggers when user scrolls down) ---
-gsap.to(".about__description, .about__img", {
+gsap.to(".features__item, .about__description, .about__img", {
   opacity: 1,
   y: 0,
   duration: 0.8,
   stagger: 0.2, // This creates the "one-after-the-other" waterfall effect!
   scrollTrigger: {
-    trigger: ".about",
+    trigger: ".features",
     start: "top 75%", // Starts when the top of the section hits 75% of the viewport height
     toggleActions: "play none none none", // Plays once and stays visible
   },
+});
+
+//HAMBURGER
+const openMenu = document.querySelector(".fa-bars");
+const closeMenu = document.querySelector(".fa-xmark");
+const nav = document.querySelector(".header__wrapper");
+const navLinks = document.querySelector(".header__nav-links");
+
+openMenu.addEventListener("click", () => {
+  nav.classList.add("active-menu");
+  navLinks.classList.add("active-list");
+});
+
+closeMenu.addEventListener("click", () => {
+  nav.classList.remove("active-menu");
+  navLinks.classList.remove("active-list");
 });
